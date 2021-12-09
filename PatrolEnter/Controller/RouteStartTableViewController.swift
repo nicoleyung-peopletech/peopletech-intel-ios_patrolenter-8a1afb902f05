92da537d023c9c15436bee2feb1dd2d59c54c185
Nicole <goolitesum@gmail.com>
2021-12-09 18:43:20 +0800

    updated

---
+++ PatrolEnter/Controller/RouteStartTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RouteStartTableViewController.swift
+//  PatrolEnter
+//
+//  Created by Nicole on 9/12/2021.
+//
+
+import UIKit
+
+class RouteStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+
+}
